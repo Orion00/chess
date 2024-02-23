1d1091fda1847330e64863ab9f283f91e597649f
Orion Bowers <orionbowers@gmail.com>
2024-02-22 20:19:17 -0700

    Finish all Game Services tests

--- server/src/test/java/serviceTests/GameServiceTest.java
+++ server/src/test/java/serviceTests/GameServiceTest.java
@@ -45,12 +45,12 @@ class GameServiceTest {
     public void reset() {
         try {
             databaseService.clearApp();
+            auth = authDAO.createAuth(user);
         } catch (DataAccessException e) {
             throw new RuntimeException("Failed to clear the DB");
         }
 
         try {
-            auth = authDAO.createAuth(user);
             insertedGame = gameService.createGame(auth, "Game 34");
         } catch (DataAccessException i){
             System.out.println(i.getMessage());
@@ -64,38 +64,54 @@ class GameServiceTest {
     @Order(1)
     @DisplayName("Create Game - Unauthorized")
     public void createGameUnauthorized() {
-        Assertions.assertThrows(DataAccessException.class,() -> gameService.createGame(auth,"Game 34"));
+        String expectedException = "Unauthorized";
+        DataAccessException actualException = assertThrows(DataAccessException.class,() -> gameService.createGame(authWrong,"Game 34"));
+        assertEquals(expectedException,actualException.getMessage());
     }
 
     @Test
     @Order(2)
-    @DisplayName("Create Game - Authorized")
-    public void createGameAuthorized() {
-        GameData expected = new GameData(0,null,null,"Game 34", null);
-        auth = authDAO.createAuth(user);
-        GameData actual = Assertions.assertDoesNotThrow(() -> gameService.createGame(auth,"Game 34"));
-        Assertions.assertEquals(expected.gameName(),actual.gameName());
+    @DisplayName("Create Game - Already Exists")
+    public void createGameAlreadyExists() {
+        String expectedException = "Game already exists";
+        DataAccessException actualException = assertThrows(DataAccessException.class,() -> gameService.createGame(auth,"Game 34"));
+        assertEquals(expectedException,actualException.getMessage());
     }
 
     @Test
     @Order(3)
-    @DisplayName("List Games - Unauthorized")
-    public void ListGamesUnauthorized() {
-        Assertions.assertThrows(DataAccessException.class,() -> gameService.ListGames(auth));
+    @DisplayName("Create Game - Authorized")
+    public void createGameAuthorized() {
+        GameData expected = new GameData(0,null,null,"Game 0", null);
+        auth = authDAO.createAuth(user);
+        GameData actual = Assertions.assertDoesNotThrow(() -> gameService.createGame(auth,"Game 0"));
+        Assertions.assertEquals(expected.gameName(),actual.gameName());
     }
 
     @Test
     @Order(4)
-    @DisplayName("List Games - Authorized")
-    public void ListGamesAuthorized() {
-        List<GameData> expected = new ArrayList<>();
-        expected.add(insertedGame);
-        List<GameData> actual = Assertions.assertDoesNotThrow(() ->gameService.ListGames(auth));
-        Assertions.assertEquals(expected,actual);
+    @DisplayName("List Games - Unauthorized")
+    public void ListGamesUnauthorized() {
+        String expectedException = "Unauthorized";
+        DataAccessException actualException = assertThrows(DataAccessException.class,() -> gameService.ListGames(authWrong));
+        assertEquals(expectedException,actualException.getMessage());
     }
 
     @Test
     @Order(5)
+    @DisplayName("List Games - Authorized")
+    public void ListGamesAuthorized() {
+        GameData insertedGame2 = Assertions.assertDoesNotThrow(() -> gameService.createGame(auth, "Game 35"));
+
+        List<GameData> expected = new ArrayList<>();
+        expected.add(insertedGame);
+        expected.add(insertedGame2);
+        List<GameData> actual = Assertions.assertDoesNotThrow(() -> gameService.ListGames(auth));
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    @Order(6)
     @DisplayName("List Games - Empty")
     public void ListGamesEmpty() {
         try {
@@ -111,26 +127,51 @@ class GameServiceTest {
     }
 
     @Test
-    @Order(6)
+    @Order(7)
     @DisplayName("Join Game - Unauthorized")
     public void JoinGameUnauthorized() {
-        Assertions.assertThrows(DataAccessException.class,() ->gameService.joinGame(auth,"WHITE",404));
-    }
-
-    @Test
-    @Order(7)
-    @DisplayName("Join Game - Incorrect GameID")
-    public void JoinGameWrongID() {
-        auth = authDAO.createAuth(user);
-        Assertions.assertThrows(DataAccessException.class,() ->gameService.joinGame(auth,"WHITE",404));
+        String expectedException = "Unauthorized";
+        DataAccessException actualException =assertThrows(DataAccessException.class,() ->gameService.joinGame(authWrong,"WHITE",34));
+        assertEquals(expectedException,actualException.getMessage());
     }
 
     @Test
     @Order(8)
+    @DisplayName("Join Game - Incorrect GameID")
+    public void JoinGameWrongID() {
+        auth = authDAO.createAuth(user);
+        String expectedException = "GameID doesn't exist";
+        DataAccessException actualException = assertThrows(DataAccessException.class,() ->gameService.joinGame(auth,"WHITE",404));
+        assertEquals(expectedException,actualException.getMessage());
+    }
+
+    @Test
+    @Order(9)
+    @DisplayName("Join Game - Color Already Used")
+    public void JoinGameColorAlreadyUsed() {
+        auth = authDAO.createAuth(user);
+        Assertions.assertDoesNotThrow(() ->gameService.joinGame(auth,"WHITE",insertedGame.gameID()));
+        String expectedException = "WHITE is already taken in this game";
+        DataAccessException actualException = assertThrows(DataAccessException.class,() ->gameService.joinGame(auth,"WHITE",insertedGame.gameID()));
+        assertEquals(expectedException,actualException.getMessage());
+    }
+
+    @Test
+    @Order(10)
+    @DisplayName("Join Game - Invalid Color")
+    public void JoinGameInvalidColor() {
+        auth = authDAO.createAuth(user);
+        String expectedException = "Invalid color entered";
+        DataAccessException actualException = assertThrows(DataAccessException.class,() ->gameService.joinGame(auth,"MAGENTA",insertedGame.gameID()));
+        assertEquals(expectedException,actualException.getMessage());
+    }
+
+    @Test
+    @Order(11)
     @DisplayName("Join Game - Authorized")
     public void JoinGameAuthorized() {
         auth = authDAO.createAuth(user);
-        Assertions.assertThrows(DataAccessException.class,() ->gameService.joinGame(auth,"WHITE",404));
+        Assertions.assertDoesNotThrow(() ->gameService.joinGame(auth,"WHITE",insertedGame.gameID()));
     }
 
 
