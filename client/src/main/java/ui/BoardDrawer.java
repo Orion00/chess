9f8d89f153f1fedacb9308e4f695a62a3d53c6f4
Orion Bowers <orionbowers@gmail.com>
2024-03-21 19:54:47 -0600

    Finish gameUI basically

--- client/src/main/java/ui/BoardDrawer.java
+++ client/src/main/java/ui/BoardDrawer.java
@@ -4,13 +4,8 @@ import chess.ChessBoard;
 import chess.ChessGame;
 import chess.ChessPiece;
 import exception.ResponseException;
-import model.GameData;
-
 import java.io.PrintStream;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+
 
 import static ui.EscapeSequences.*;
 
@@ -19,32 +14,29 @@ public class BoardDrawer {
     private static final int SQUARE_SIZE_IN_CHARS = 1;
     private static final int LINE_WIDTH_IN_CHARS = 1;
     private static final String EMPTY = "   ";
-    private static final String X = " X ";
-    private static final String O = " O ";
-    private static Random rand = new Random();
     private static String playerColor;
 
     private static ChessBoard board;
 
     public void drawBoard(PrintStream out, String playerColor, ChessBoard board) throws ResponseException {
-        BoardDrawer.playerColor = playerColor;
-        BoardDrawer.board = board;
-        out.print(ERASE_SCREEN);
+        try {
+            BoardDrawer.playerColor = playerColor;
+            BoardDrawer.board = board;
+            out.print(ERASE_SCREEN);
 
-        drawHeaders(out);
+            drawHeaders(out);
+            drawSquares(out);
+            drawHeaders(out);
 
-        drawSquares(out);
-
-        out.print(SET_BG_COLOR_BLACK);
-        out.print(SET_TEXT_COLOR_WHITE);
+            resetBGAndTextCol(out);
+            out.println();
+        } catch (ResponseException i) {
+            resetBGAndTextCol(out);
+            throw i;
+        }
     }
     private static void drawHeaders(PrintStream out) throws ResponseException {
-
-        setBlack(out);
-        setFontSize(out,30);
-
         String[] headers;
-
         if (playerColor.equals("WHITE")) {
             headers = new String[]{"a", "b", "c", "d", "e", "f", "g", "h"};
         } else if (playerColor.equals("BLACK")) {
@@ -52,41 +44,43 @@ public class BoardDrawer {
         } else {
             throw new ResponseException(400, "Incorrect player color somehow");
         }
+
+        drawHeader(out, " ");
         for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
             drawHeader(out, headers[boardCol]);
-
-            if (boardCol < BOARD_SIZE_IN_SQUARES - 1) {
-                out.print(EMPTY.repeat(LINE_WIDTH_IN_CHARS));
-            }
         }
+        drawHeader(out, " ");
 
+        out.print(RESET_BG_COLOR);
         out.println();
-        resetFontSize(out);
     }
 
     private static void drawHeader(PrintStream out, String headerText) {
-        int prefixLength = SQUARE_SIZE_IN_CHARS / 2;
-        int suffixLength = SQUARE_SIZE_IN_CHARS - prefixLength - 1;
-
-        out.print(EMPTY.repeat(prefixLength));
-        printHeaderText(out, headerText);
-        out.print(EMPTY.repeat(suffixLength));
-    }
-
-    private static void printHeaderText(PrintStream out, String player) {
-        out.print(SET_BG_COLOR_BLACK);
-        out.print(SET_TEXT_COLOR_GREEN);
-
-        out.print(player);
-
         setBlack(out);
+        out.print(SET_TEXT_COLOR_CREME);
+        out.print(" ");
+        out.print(headerText);
+        out.print(" ");
     }
 
-    private static void drawSquares(PrintStream out) {
+    private static void drawSquares(PrintStream out) throws ResponseException {
+        int startRow;
+        int endRow;
+        int increment;
+        if (playerColor.equals("BLACK")) {
+            startRow = 0;
+            endRow = BOARD_SIZE_IN_SQUARES;
+            increment = 1;
+        } else {
+            startRow = BOARD_SIZE_IN_SQUARES-1;
+            endRow =-1;
+            increment = -1;
+        }
 
-        for (int boardRow = 0; boardRow < BOARD_SIZE_IN_SQUARES; ++boardRow) {
+        for (int boardRow = startRow; boardRow != endRow; boardRow += increment) {
             ChessPiece[] boardRowValues = board.getBoard()[boardRow];
 
+            drawRowHeaders(out, boardRow);
             for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
                 if ((boardCol % 2 == 0 && boardRow % 2 == 0) ||
                         (boardCol % 2 != 0 && boardRow % 2 != 0)) {
@@ -96,9 +90,8 @@ public class BoardDrawer {
                 }
                 out.print(getPieceSymbol(boardRowValues[boardCol]));
             }
-            setBlack(out);
-            out.print("a");
-            out.print(RESET_BG_COLOR);
+            drawRowHeaders(out, boardRow);
+            resetBGAndTextCol(out);
             out.println();
 
 //            if (boardRow < BOARD_SIZE_IN_SQUARES - 1) {
@@ -106,46 +99,9 @@ public class BoardDrawer {
 //                setBlack(out);
 //            }
         }
-        out.println();
-    }
-
-    private static void drawRowOfSquares(PrintStream out, ChessPiece[] row) {
-        for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
-            if (boardCol % 2 == 0) {
-                setCreme(out);
-            } else {
-                setBrown(out);
-            }
-            out.print(getPieceSymbol(row[boardCol]));
-        }
-//        for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
-//            setCreme(out);
-//
-//            if (squareRow == SQUARE_SIZE_IN_CHARS / 2) {
-//                int prefixLength = SQUARE_SIZE_IN_CHARS / 2;
-//                int suffixLength = SQUARE_SIZE_IN_CHARS - prefixLength - 1;
-//
-//                out.print(EMPTY.repeat(prefixLength));
-//
-//
-//                printPlayer(out, rand.nextBoolean() ? X : O);
-//                out.print(EMPTY.repeat(suffixLength));
-//            }
-//            else {
-//                out.print(EMPTY.repeat(SQUARE_SIZE_IN_CHARS));
-//            }
-//
-//            if (boardCol < BOARD_SIZE_IN_SQUARES - 1) {
-//                // Draw right line
-//                setBrown(out);
-//                out.print(EMPTY.repeat(LINE_WIDTH_IN_CHARS));
-//            }
-//
-//            setBlack(out);
-//        }
-//
 //        out.println();
     }
+
 
     private static String getPieceSymbol(ChessPiece chessPiece) {
         ChessPiece.PieceType type;
@@ -170,30 +126,13 @@ public class BoardDrawer {
         };
     }
 
-    private static void drawVerticalLine(PrintStream out) {
-
-        int boardSizeInSpaces = BOARD_SIZE_IN_SQUARES * SQUARE_SIZE_IN_CHARS +
-                (BOARD_SIZE_IN_SQUARES - 1) * LINE_WIDTH_IN_CHARS;
-
-        for (int lineRow = 0; lineRow < LINE_WIDTH_IN_CHARS; ++lineRow) {
-            setBrown(out);
-            out.print(EMPTY.repeat(boardSizeInSpaces));
-
-            setBlack(out);
-            out.println();
-        }
-    }
 
     private static void setCreme(PrintStream out) {
-//        out.print(SET_BG_COLOR_WHITE);
-//        out.print(SET_TEXT_COLOR_WHITE);
         out.print(SET_BG_COLOR_CREME);
         out.print(SET_TEXT_COLOR_BLACK);
     }
 
     private static void setBrown(PrintStream out) {
-//        out.print(SET_BG_COLOR_RED);
-//        out.print(SET_TEXT_COLOR_RED);
         out.print(SET_BG_COLOR_BROWN);
         out.print(SET_TEXT_COLOR_BLACK);
     }
@@ -213,12 +152,21 @@ public class BoardDrawer {
         out.print(RESET_FONT_SIZE);
     }
 
-    private static void printPlayer(PrintStream out, String player) {
-        out.print(SET_BG_COLOR_WHITE);
-        out.print(SET_TEXT_COLOR_BLACK);
 
-        out.print(player);
+    private static void resetBGAndTextCol(PrintStream out) {
+        out.print(RESET_BG_COLOR);
+        out.print(RESET_TEXT_COLOR);
+    }
 
-        setCreme(out);
+    private static void drawRowHeaders(PrintStream out, Integer row) throws ResponseException {
+        String[] headers = new String[]{"1", "2", "3", "4", "5", "6", "7", "8"};
+        drawHeader(out, headers[row]);
+    }
+
+    private static void setTextColor(PrintStream out,int r, int g, int b) {
+        out.printf("\u001B[38;2;%d;%d;%dm", r, g, b);
+    }
+    private static void setBackgroundColor(PrintStream out,int r, int g, int b) {
+        out.printf("\u001B[48;2;%d;%d;%dm", r, g, b);
     }
 }
