1046e3ab1be4e8dd9152ba632309d257d57185be
Orion Bowers <orionbowers@gmail.com>
2024-04-11 18:56:46 -0600

    Rework duplicated code

--- server/src/main/java/dataAccess/DBUserDAO.java
+++ server/src/main/java/dataAccess/DBUserDAO.java
@@ -68,18 +68,8 @@ public class DBUserDAO implements UserDAO {
     }
 
     private void executeUpdate(String statement, Object... params) throws DataAccessException {
-        try (var conn = DatabaseManager.getConnection()) {
-            try (var ps = conn.prepareStatement(statement)) {
-                for (var i = 0; i < params.length; i++) {
-                    var param = params[i];
-                    if (param instanceof String p) ps.setString(i + 1, p);
-                    else if (param == null) ps.setNull(i + 1, NULL);
-                }
-                ps.executeUpdate();
-            }
-        } catch (SQLException e) {
-            throw new DataAccessException(String.format("unable to update database: %s, %s", statement, e.getMessage()));
-        }
+        ExecuteUpdater exe = new ExecuteUpdater();
+        exe.executeUpdate(statement, params);
     }
 
     // Has DBManager create the DB if needed
---
+++ server/src/main/java/dataAccess/ExecuteUpdater.java
@@ -0,0 +1,22 @@
+package dataAccess;
+
+import java.sql.SQLException;
+
+import static java.sql.Types.NULL;
+
+public class ExecuteUpdater {
+    public void executeUpdate(String statement, Object... params) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(statement)) {
+                for (var i = 0; i < params.length; i++) {
+                    var param = params[i];
+                    if (param instanceof String p) ps.setString(i + 1, p);
+                    else if (param == null) ps.setNull(i + 1, NULL);
+                }
+                ps.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(String.format("unable to update database: %s, %s", statement, e.getMessage()));
+        }
+    }
+}
